6b5d10b60312928ca293b4eddaf7d349beb918bb
kunal <kunal770609@gmail.com>
2024-01-18 15:23:37 +0530

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Index from '@/app/pages/index'
+
+const Home = () => {
+  return (
+    <div>
+    <Index/>
+    </div>
+  )
+}
+
+export default Home
+
---
+++ src/components/subPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const subPage = () => {
+  return (
+    <div>
+this is sub page 
+    </div>
+  )
+}
+
+export default subPage
+
